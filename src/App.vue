ba2318f5b3e764e7ef85e91b287e378d68c106ae
simone <sdiricco.91@gmail.com>
2024-03-15 18:22:54 +0100

    add some fix

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,18 @@
   <div ref="viewerContainer" class="pdf-viewer"></div>
 </template>
 
-<script>
-export default {
-  mounted() {
-    // Carica viewer.html
-    const viewerHtmlPath = 'pdfjs-4.0.379-legacy-dist/web/viewer.html'; // Assicurati di specificare il percorso corretto
-    const iframe = document.createElement('iframe');
-    iframe.src = viewerHtmlPath;
-    iframe.style.width = '100%';
-    iframe.style.height = '100%'; 
-    iframe.style.borderWidth = '0px';
-    this.$refs.viewerContainer.appendChild(iframe);
-  }
-};
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+const viewerContainer = ref<any>(null)
+onMounted(() => {
+  const viewerHtmlPath = 'pdfjs-4.0.379-legacy-dist/web/viewer.html'; // Assicurati di specificare il percorso corretto
+  const iframe = document.createElement('iframe');
+  iframe.src = viewerHtmlPath;
+  iframe.style.width = '100%';
+  iframe.style.height = '100%'; 
+  iframe.style.borderWidth = '0px';
+  viewerContainer.value.appendChild(iframe);
+})
 </script>
 
 <style scoped>
