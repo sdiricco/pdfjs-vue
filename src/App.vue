5c23e11fe175d294317bd63d57fe4ca199b8556e
simone <sdiricco.91@gmail.com>
2024-03-15 18:15:05 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div ref="viewerContainer" class="pdf-viewer"></div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    // Carica viewer.html
+    const viewerHtmlPath = 'pdfjs-4.0.379-legacy-dist/web/viewer.html'; // Assicurati di specificare il percorso corretto
+    const iframe = document.createElement('iframe');
+    iframe.src = viewerHtmlPath;
+    iframe.style.width = '100%';
+    iframe.style.height = '100%'; 
+    iframe.style.borderWidth = '0px';
+    this.$refs.viewerContainer.appendChild(iframe);
+  }
+};
+</script>
+
+<style scoped>
+.pdf-viewer {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
